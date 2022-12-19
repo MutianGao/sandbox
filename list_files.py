5eb8481c3c3cb4cee3a5a2df975a10493c16ab4d
mutian gao JCU <102776257+MutianGao@users.noreply.github.com>
2022-12-19 21:32:50 +0800

    Update list_files.py

--- list_files.py
+++ list_files.py
@@ -4,3 +4,10 @@ print("The files and folders in {} are:".format(os.getcwd()))
 items = os.listdir('.')
 for item in items:
     print(item)
+   
+import os
+
+print("The files and folders in {} are:".format(os.getcwd()))
+items = os.listdir('.')
+for item in items:
+    print(item)
